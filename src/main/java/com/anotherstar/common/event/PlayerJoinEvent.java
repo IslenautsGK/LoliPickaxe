ad2e4726b1ebf43602ba4d971e12fc8466a738d7
IslenautsGK <dhsth@outlook.com>
2019-11-30 16:46:08 +0800

    add join info

--- src/main/java/com/anotherstar/common/event/PlayerJoinEvent.java
+++ src/main/java/com/anotherstar/common/event/PlayerJoinEvent.java
@@ -5,7 +5,10 @@ import com.anotherstar.common.config.ConfigLoader;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.event.ClickEvent;
+import net.minecraft.event.HoverEvent;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
 
 public class PlayerJoinEvent {
 
@@ -13,8 +16,15 @@ public class PlayerJoinEvent {
 	public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
 		if (event.player instanceof EntityPlayerMP) {
 			ConfigLoader.sandChange((EntityPlayerMP) event.player);
-			event.player.addChatComponentMessage(new ChatComponentText("LoliPickaxe开源地址:"));
-			event.player.addChatComponentMessage(new ChatComponentText("https://github.com/IslenautsGK/LoliPickaxe"));
+			ChatComponentText message = new ChatComponentText("§2LoliPickaxe§f开源地址: ");
+			ChatComponentTranslation submsg = new ChatComponentTranslation(
+					"§9§nhttps://github.com/IslenautsGK/LoliPickaxe");
+			submsg.getChatStyle()
+					.setChatClickEvent(
+							new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/IslenautsGK/LoliPickaxe"))
+					.setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText("点击前往链接")));
+			message.appendSibling(submsg);
+			event.player.addChatComponentMessage(message);
 		}
 	}
 
